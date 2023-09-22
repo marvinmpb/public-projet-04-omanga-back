07ce92a1be5ea18c0ee121f94b1ce60b9d23d559
marvinmpb <mpbmarvin@gmail.com>
2023-09-22 08:42:40 +0200

    removed console.log

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -17,7 +17,6 @@ module.exports = {
         where: { id: parseInt(id) },
         include: { reviews: true, orders: true },
       });
-      console.log(result);
       res.status(200).json(result);
     } catch (error) {
       res.status(400).json({ error: error.message });
