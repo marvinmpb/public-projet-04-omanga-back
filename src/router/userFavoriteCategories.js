70242c625c3bfaed3daed7999e5a8c519e941484
marvinmpb <mpbmarvin@gmail.com>
2023-10-04 20:35:18 +0200

    WIP => added protection that requires authentication on certain routes and added a check to verify that the user connected is the one who wants to make the requests

--- src/router/userFavoriteCategories.js
+++ src/router/userFavoriteCategories.js
@@ -1,13 +1,14 @@
 const asyncHelper = require('../helpers/async');
 const favoriteCategoriesController = require('../controllers/favoriteCategories');
 const { Router } = require('express');
+const authenticate = require('../middlewares/authenticate');
 const router = Router();
 
-router.post('/:userId/favorite/categories/:categoryId', asyncHelper(favoriteCategoriesController.addOneFavoriteCategory));
+router.post('/:userId/favorite/categories/:categoryId', authenticate, asyncHelper(favoriteCategoriesController.addOneFavoriteCategory));
 
-router.get('/:userId/favorite/categories', asyncHelper(favoriteCategoriesController.getAllFavoriteCategories));
+router.get('/:userId/favorite/categories', authenticate, asyncHelper(favoriteCategoriesController.getAllFavoriteCategories));
 
-router.delete('/:userId/favorite/categories/:categoryId', asyncHelper(favoriteCategoriesController.removeOneFavoriteCategory));
+router.delete('/:userId/favorite/categories/:categoryId', authenticate, asyncHelper(favoriteCategoriesController.removeOneFavoriteCategory));
 
 
 module.exports = router;
