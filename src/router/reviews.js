1ad1290d8074932f747ae6dca1e90c328be8aa3b
marvinmpb <mpbmarvin@gmail.com>
2023-10-05 00:02:06 +0200

    added schemas validation on routes

--- src/router/reviews.js
+++ src/router/reviews.js
@@ -1,16 +1,18 @@
 const asyncHelper = require('../helpers/async');
 const reviewsController = require('../controllers/reviews');
+const validation = require('../middlewares/validate');
+const schema = require('../schemas/reviews');
 const { Router } = require('express');
 const authenticate = require('../middlewares/authenticate');
 const router = Router();
 
-router.post('/users/:id/products/:productId', authenticate, asyncHelper(reviewsController.addReview));
+router.post('/users/:id/products/:productId', authenticate, validation(schema.create, 'body'), asyncHelper(reviewsController.addReview));
 
 router.get('/products/:productId', asyncHelper(reviewsController.getAllReviewsByProduct));
 
 router.get('/reviews/:reviewId', asyncHelper(reviewsController.getOneReview));
 
-router.put('/users/:id/reviews/:reviewId', authenticate, asyncHelper(reviewsController.updateOneReview));
+router.put('/users/:id/reviews/:reviewId', authenticate, validation(schema.create, 'body'), asyncHelper(reviewsController.updateOneReview));
 
 router.delete('/users/:id/reviews/:reviewId', authenticate, asyncHelper(reviewsController.deleteOneReview));
 
