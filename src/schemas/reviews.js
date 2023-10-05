79553d0a0e926d87c84b3d726be0a475311fd329
marvinmpb <mpbmarvin@gmail.com>
2023-10-05 12:54:29 +0200

    WIP => add createUsers function inside mockData folder in order to seed the database with fake users data

--- src/schemas/reviews.js
+++ src/schemas/reviews.js
@@ -12,7 +12,7 @@ module.exports = {
 
   update: joi.object({
     order_date: joi.date().iso(),
-    archeving_date: joi.string().iso(),
+    archeving_date: joi.date().iso(),
     product_quantity: joi.number().integer(),
     product_id: joi.number().integer(),
     user_id: joi.number().integer()
